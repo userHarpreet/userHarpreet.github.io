6d3ab2faee0e100c160f3d42ca48613664b91f3f
Harpreet <user.harpreetsingh@outlook.com>
2024-06-09 18:37:15 +0530

    Updates

---
+++ component---src-pages-404-js-d8bb872bd51114be5c3e.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{w2l6:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),l=n.n(a),u=n("1Qp6");t.default=()=>l.a.createElement(u.c,null,l.a.createElement(u.b,null,l.a.createElement(u.d,{title:"404: Not found"}),l.a.createElement("h1",null,"NOT FOUND"),l.a.createElement("p",null,"OOPS! You just hit a page that doesn't exist...")))}}]);
+//# sourceMappingURL=component---src-pages-404-js-d8bb872bd51114be5c3e.js.map
